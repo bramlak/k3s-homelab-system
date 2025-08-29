668f9b9fe4251dd5add6af4ae92353ab08387145
bramlak <kalmarbalint97@gmail.com>
2025-08-29 13:46:40 +0200

    add kubernetes provider setup

---
+++ k3s/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/k3s-homelab/config"
+}
+
