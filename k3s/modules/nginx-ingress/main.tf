c1f02c495ca261ebecdbd94462ffd79f7f7d99d4
bramlak <kalmarbalint97@gmail.com>
2025-09-30 19:47:18 +0200

    feat(ingress): add vaultwarden ingress

--- k3s/modules/nginx-ingress/main.tf
+++ k3s/modules/nginx-ingress/main.tf
@@ -12,3 +12,41 @@ resource "helm_release" "nginx_ingress" {
   namespace  = kubernetes_namespace.nginx_ingress.metadata[0].name
 }
 
+
+
+resource "kubernetes_ingress_v1" "vaultwarden" {
+  metadata {
+    name      = "vaultwarden"
+    namespace  = var.vaultwarden_namespace
+    annotations = {
+      "kubernetes.io/ingress.class" = "nginx"
+    }
+  }
+
+  spec {
+    ingress_class_name = "nginx"
+    tls {
+      hosts      = ["vaultwarden.local"]
+      secret_name = "vaultwarden-tls"
+    }
+
+    rule {
+      host = "cirrus"
+      http {
+        path {
+          path     = "/"
+          path_type = "Prefix"
+          backend {
+            service {
+              name = "vaultwarden"
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
