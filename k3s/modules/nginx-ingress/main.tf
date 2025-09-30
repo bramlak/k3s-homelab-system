2ac99b5fe7a0de2aa4cca9828b8f7005608b6d7e
bramlak <kalmarbalint97@gmail.com>
2025-09-30 19:41:50 +0200

    feat(helm-release): add nginx-ingress helm release

--- k3s/modules/nginx-ingress/main.tf
+++ k3s/modules/nginx-ingress/main.tf
@@ -5,3 +5,10 @@ resource "kubernetes_namespace" "nginx_ingress" {
   }
 }
 
+resource "helm_release" "nginx_ingress" {
+  name       = "nginx-ingress"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+  namespace  = kubernetes_namespace.nginx_ingress.metadata[0].name
+}
+
