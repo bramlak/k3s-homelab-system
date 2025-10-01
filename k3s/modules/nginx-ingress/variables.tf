e5b41dd9ceea5b4929ac03d090a8e0abcb39f55a
bramlak <kalmarbalint97@gmail.com>
2025-10-01 21:01:26 +0200

    feat(ingress): create variables for syncthing ingress

--- k3s/modules/nginx-ingress/variables.tf
+++ k3s/modules/nginx-ingress/variables.tf
@@ -8,6 +8,16 @@ variable "vaultwarden_service_port" {
   type = number
 }
 
+variable "syncthing_namespace" {
+  description = "syncthing namespace"
+  type  = string
+}
+
+variable "syncthing_service_port" {
+  description = "syncthing service port"
+  type = number
+}
+
 variable "ingress_host_name" {
   description = "ingress host name"
   type = string
