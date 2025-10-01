2a17f20f77fcc5c29eb5343d2dd431af96f6c8f6
bramlak <kalmarbalint97@gmail.com>
2025-10-01 20:50:11 +0200

    feat(deployment): create syncthing deployment

--- k3s/modules/syncthing/main.tf
+++ k3s/modules/syncthing/main.tf
@@ -46,3 +46,88 @@ resource "kubernetes_persistent_volume_claim" "syncthing" {
     volume_name        = kubernetes_persistent_volume.syncthing.metadata[0].name
   }
 }
+
+
+resource "kubernetes_deployment" "syncthing" {
+  metadata {
+    name      = "syncthing"
+    namespace = kubernetes_namespace.syncthing.metadata[0].name
+    labels = {
+      app = "syncthing"
+    }
+  }
+
+  spec {
+    replicas = var.replicas
+    selector {
+      match_labels = {
+        app = "syncthing"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "syncthing"
+        }
+      }
+
+      spec {
+        container {
+          name  = "syncthing"
+          image = "syncthing/syncthing:${var.image_version}"
+
+          volume_mount {
+            name       = "syncthing-data"
+            mount_path = "/var/syncthing"
+          }
+
+          port {
+            container_port = 8384
+          }
+          port {
+            container_port = 22000
+          }
+          port {
+            container_port = 21027
+            protocol      = "UDP"
+          }
+
+          env_from {
+            secret_ref {
+              name = kubernetes_secret.syncthing.metadata[0].name
+            }
+          }
+
+          resources {
+            requests = {
+              cpu    = var.cpu_request
+              memory = var.memory_request
+            }
+            limits = {
+              cpu    = var.cpu_limit
+              memory = var.memory_limit
+            }
+          }
+
+          liveness_probe {
+            http_get {
+              path = "/"
+              port = 8384
+            }
+            initial_delay_seconds = 30
+            period_seconds        = 10
+          }
+        }
+
+        volume {
+          name = "syncthing-data"
+          persistent_volume_claim {
+            claim_name = kubernetes_persistent_volume_claim.syncthing.metadata[0].name
+          }
+        }
+      }
+    }
+  }
+}
+
