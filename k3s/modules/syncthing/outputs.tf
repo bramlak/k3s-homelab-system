d25bc6679684b98d8b2d78230a3b6f9efc5a58be
bramlak <kalmarbalint97@gmail.com>
2025-10-01 20:58:24 +0200

    feat(service): create output for syncthing service_port

--- k3s/modules/syncthing/outputs.tf
+++ k3s/modules/syncthing/outputs.tf
@@ -2,3 +2,8 @@ output "namespace" {
   description = "Kubernetes namespace for Syncthing"
   value       = kubernetes_namespace.syncthing.metadata[0].name
 }
+
+output "service_port" {
+  value = var.service_port
+  description = "Service port of the Vaultwarden deployment"
+}
