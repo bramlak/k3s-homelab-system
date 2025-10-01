415664096a00b83889f5b14c147b93e90e996746
bramlak <kalmarbalint97@gmail.com>
2025-10-01 12:49:33 +0200

    feat(deployment): create variables for vaultwarden deployment

--- k3s/modules/vaultwarden/variables.tf
+++ k3s/modules/vaultwarden/variables.tf
@@ -3,3 +3,57 @@ variable "namespace" {
   type        = string
   default     = "vaultwarden"
 }
+
+variable "image_version" {
+  description = "Vaultwarden image version"
+  type        = string
+  default     = "1.30.1"
+}
+
+variable "replicas" {
+  description = "Number of Vaultwarden replicas"
+  type        = number
+  default     = 1
+}
+
+variable "admin_token" {
+  description = "Admin token for Vaultwarden"
+  type        = string
+  sensitive   = true
+}
+
+variable "ingress_port" {
+  description = "Port for the Ingress controller"
+  type        = number
+  default     = 30080
+}
+
+variable "ingress_host" {
+  description = "Hostname for the Ingress controller"
+  type        = string
+  default     = "cirrus"
+}
+
+variable "cpu_request" {
+  description = "CPU request for Vaultwarden container"
+  type        = string
+  default     = "100m"
+}
+
+variable "memory_request" {
+  description = "Memory request for Vaultwarden container"
+  type        = string
+  default     = "256Mi"
+}
+
+variable "cpu_limit" {
+  description = "CPU limit for Vaultwarden container"
+  type        = string
+  default     = "500m"
+}
+
+variable "memory_limit" {
+  description = "Memory limit for Vaultwarden container"
+  type        = string
+  default     = "512Mi"
+}
