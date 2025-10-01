cd0e1a166e3780f69626a5c7f06cba1497e49bb5
bramlak <kalmarbalint97@gmail.com>
2025-10-01 12:56:57 +0200

    feat(module): pass variables to vaultwarden module

--- k3s/main.tf
+++ k3s/main.tf
@@ -14,4 +14,17 @@ module "nginx_ingress" {
 
 module "vaultwarden" {
   source = "../modules/vultwarden"
+  namespace      = var.namespace
+  image_version  = var.image_version
+  replicas       = var.replicas
+  admin_token    = var.admin_token
+  service_port   = var.service_port
+  ingress_port   = var.ingress_port
+  ingress_host   = var.ingress_host
+  cpu_request    = var.cpu_request
+  memory_request = var.memory_request
+  cpu_limit      = var.cpu_limit
+  memory_limit   = var.memory_limit
+  tls_crt        = var.tls_crt
+  tls_key        = var.tls_key
 }
