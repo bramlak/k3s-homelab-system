3c3e8547ecb0fb6c821dd85f63216d9800653c85
bramlak <kalmarbalint97@gmail.com>
2025-10-01 12:38:51 +0200

    feat(pvc): create pvc for vaultwarden

--- k3s/modules/vaultwarden/main.tf
+++ k3s/modules/vaultwarden/main.tf
@@ -3,3 +3,24 @@ resource "kubernetes_namespace" "vaultwarden" {
     name = var.namespace
   }
 }
+
+
+resource "kubernetes_persistent_volume_claim" "vaultwarden" {
+  metadata {
+    name      = "vaultwarden-pvc"
+    namespace = kubernetes_namespace.vaultwarden.metadata[0].name
+  }
+
+  spec {
+    access_modes = ["ReadWriteOnce"]
+    resources {
+      requests = {
+        storage = "1Gi"
+      }
+    }
+
+    storage_class_name = "manual"
+    volume_name        = kubernetes_persistent_volume.vaultwarden.metadata[0].name
+  }
+
+}
