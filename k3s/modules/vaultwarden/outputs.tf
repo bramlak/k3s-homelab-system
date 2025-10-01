77a7a24368478ae7bf19e067189380da87249d8a
bramlak <kalmarbalint97@gmail.com>
2025-10-01 12:54:25 +0200

    feat(namespace): create namespace output

--- k3s/modules/vaultwarden/outputs.tf
+++ k3s/modules/vaultwarden/outputs.tf
@@ -2,3 +2,8 @@ output "service_url" {
   value       = "http://${kubernetes_service.vaultwarden.metadata[0].name}.${var.namespace}.svc.cluster.local:${var.service_port}"
   description = "Vaultwarden service URL within the cluster"
 }
+
+output "namespace" {
+  value       = kubernetes_namespace.vaultwarden.metadata[0].name
+  description = "Vaultwarden namespace"
+}
