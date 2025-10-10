f11996a945f6d7b65becba386cbe1915591b2c7c
bramlak <kalmarbalint97@gmail.com>
2025-10-10 16:25:51 +0200

    feat(pvc): add persistent volume for grafana

--- k3s/modules/grafana/main.tf
+++ k3s/modules/grafana/main.tf
@@ -3,3 +3,51 @@ resource "kubernetes_namespace" "grafana" {
     name = var.namespace
   }
 }
+
+
+resource "kubernetes_persistent_volume_claim" "grafana" {
+  metadata {
+    name      = "grafana-pvc"
+    namespace = kubernetes_namespace.grafana.metadata[0].name
+  }
+
+  spec {
+    access_modes = ["ReadWriteOnce"]
+    resources {
+      requests = {
+        storage = "1Gi"
+      }
+    }
+
+    storage_class_name = "manual"
+    volume_name        = kubernetes_persistent_volume.grafana.metadata[0].name
+  }
+}
+
+
+
+resource "kubernetes_persistent_volume" "grafana" {
+  metadata {
+    name = "grafana-pv"
+    labels = {
+      type = "local"
+    }
+  }
+
+  spec {
+    capacity = {
+      storage = "1Gi"
+    }
+    access_modes = ["ReadWriteOnce"]
+    storage_class_name = "manual"
+    persistent_volume_reclaim_policy = "Retain"
+
+    persistent_volume_source {
+      host_path {
+        path = "/mnt/grafana-data"
+        type = "DirectoryOrCreate"
+      }
+    }
+  }
+
+}
